48e181ba657dc6fa6ada694e9ebadec9df9570ee
戒尺 <itxiao6@qq.com>
2024-07-08 11:22:38 +0800

    v1.0.4

--- src/CustomExtendServiceProvider.php
+++ src/CustomExtendServiceProvider.php
@@ -8,11 +8,15 @@ use Illuminate\Support\ServiceProvider;
 use ManoCode\CustomExtend\Command\PublishDockerConfigCommand;
 
 /**
- *
+ * 中间层服务提供者
  */
 class CustomExtendServiceProvider extends ServiceProvider
 {
-    protected array $command = [
+    protected array $commands = [
         PublishDockerConfigCommand::class
     ];
+    public function boot(): void
+    {
+        $this->commands($this->commands);
+    }
 }
